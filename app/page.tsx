7c884e8b87884c04fcda1d73f56a1fe8093f86c7
zarman-oss <zarmanimran@gmail.com>
2024-07-17 12:55:59 +0500

    added chart

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,16 @@
+import Search from '@/components/Search';
+import UsersTable from '@/components/UsersTable';
+import { Card, Text, Title } from '@tremor/react';
+
 export default function Home() {
-  return <main>home</main>;
+  return (
+    <main className='p-4 md:p-10 mx-auto max-w-7xl'>
+      <Title>Users</Title>
+      <Text>Users stored in database</Text>
+      <Search />
+      <Card className='mt-6'> 
+        <UsersTable/>
+      </Card>
+    </main>
+  );
 }
--- components/Chart.tsx
+++ components/Chart.tsx
@@ -1,29 +1,57 @@
 'use client';
 
-import { Card, Title } from '@tremor/react';
+import { Card, Text, Title, AreaChart } from '@tremor/react';
 
 const data = [
   {
     Month: 'Jan 21',
-    Sales: 2890,
-    Profit: 2400,
+    Sales: 3000,
+    Profit: 2200,
+    Unsubscribed: 50,
   },
   {
     Month: 'Feb 21',
-    Sales: 1890,
-    Profit: 1398,
+    Sales: 2100,
+    Profit: 1700,
+    Unsubscribed: 70,
   },
   {
     Month: 'Mar 21',
-    Sales: 3890, 
-    Profit: 2980,
+    Sales: 4500,
+    Profit: 3800,
+    Unsubscribed: 30,
+  },
+  {
+    Month: 'Apr 21',
+    Sales: 3200,
+    Profit: 2900,
+    Unsubscribed: 40,
+  },
+  {
+    Month: 'May 21',
+    Sales: 5100,
+    Profit: 4100,
+    Unsubscribed: 20,
   },
 ];
 
 export default function Chart() {
   return (
-    <Card>
+    <Card className='mt-8'>
       <Title>Performance</Title>
+      <Text>Comparison between sales, profit, and unsubscribed</Text>
+      <AreaChart
+        className='mt-4 h-80'
+        data={data}
+        categories={['Sales', 'Profit', 'Unsubscribed']}
+        index='Month'
+        colors={['indigo', 'fuchsia', 'amber']}
+        valueFormatter={(number: number) =>
+          `$${Intl.NumberFormat('us').format(number)}`
+        }
+        yAxisWidth={60}
+        tooltip
+      />
     </Card>
   );
 }
